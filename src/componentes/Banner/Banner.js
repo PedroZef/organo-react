f91c8615144ce397f88cd00ae8cc3e7e3096946b
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-08-02 13:46:30 -0300

    Initial e first commit mais a inserção do logo e criação do forlulário no projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+// JSX
+import "./Banner.css"
+
+export const Banner = () => {
+  return (
+
+    <header className="banner">
+      <img src="imgs/banner.png" alt="O banner principal da página do Organo" />
+
+    </header>
+
+  );
+}
