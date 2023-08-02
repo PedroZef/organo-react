d9e37e6dae4012c87d4cebb6becffbb8bf4d2739
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-08-02 19:45:25 -0300

    Update na pasta componentes criando o Formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,17 +1,39 @@
-import "./Formulario.css";
-import  CampoTexto from "../CampoTexto";
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi submetido')
+  }
+
   return (
     <section className="formulario">
-      <form>
-        <h2>Preencha os dados para criar o card do colaborador </h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
-  );
-};
+  )
+}
 
-export default Formulario;
+export default Formulario
