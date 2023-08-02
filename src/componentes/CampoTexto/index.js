ccbc2bab2f2ab6c415561cfa2c3e9541d2ee3d69
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-08-02 14:18:54 -0300

    Update no README.md

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,15 +1,15 @@
-import "./CampoTexto.css"
+import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
-    
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModificada}/>
+            <input placeholder={placeholderModificada} />
         </div>
     )
 }
