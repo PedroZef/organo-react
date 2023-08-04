71ba26a45599a1dafb8c37498b4fd7cb32f6785e
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-08-04 20:24:24 -0300

    Corrigindo os bugs de digitação e acrescentando cores com backgroundColor

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,8 +1,8 @@
 import "./Colaborador.css"
 
-const Colaborador = ({nome, imagem, cargo}) => {
+const Colaborador = ({nome, imagem, cargo, corDeFundo }) => {
     return (
-        <div className="colaborador">
+        <div className="colaborador" style={{ backgroundColor: corDeFundo}}>
             <div className="cabecalho">
                 <img src={imagem} alt={nome}/>
             </div>
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -6,7 +6,8 @@ const ListaSuspensa = (props) => {
   return (
     <div className='lista-suspensa'>
       <label>{props.label}</label>
-      <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
+      <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+        <option value=""></option>
         {props.itens.map(item => {
           return <option key={item}>{item}</option>
         })}
